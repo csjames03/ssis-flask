03eb16910c854bb437df4e0a408cacc82e99f0ee
James Ocao <98975725+csjames03@users.noreply.github.com>
2023-11-08 13:20:36 +0800

    fix: clear all contents of app folder

--- app/routes/coursesRoutes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-courses = Blueprint('courses', __name__, static_folder="static", template_folder="templates")
-
-@courses.route('/')
-def show_students():
-    return render_template('course.html')
-
