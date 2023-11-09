c1ccbe196d5d24c60971d37acf709d200021b4bf
csjames03 <csjames0303@gmail.com>
2023-11-09 22:42:50 +0800

    feat: Initialize the routes blueprint and models for colleges and courses

---
+++ app/models/course.py
@@ -0,0 +1,9 @@
+from app import mysql
+
+
+class Courses:
+    def get_courses(self):
+        cursor = mysql.new_cursor(dictionary=True)
+        cursor.execute("SELECT * FROM course")
+        data = cursor.fetchall()
+        return data
