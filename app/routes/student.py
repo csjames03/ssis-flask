74ec7e02c5efc0801e3f09502bea61454557564d
csjames03 <csjames0303@gmail.com>
2023-11-10 14:58:52 +0800

    feat: Implement route and model for add student

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from app.models.student import Students
 
 students = Blueprint(
@@ -14,11 +14,20 @@ def show_students():
     return render_template("student.html", students=students)
 
 
-@students.route("/getStudentIds", methods=["POST"])
-def getStudentIds():
-    try:
-        students_ids = students_model.get_students_ids()
-        print(students_ids)
-        return {"messages": "Successfully retrieved"}, 200
-    except Exception as e:
-        return {"messages": str(e)}, 400
+@students.route("/add", methods=["POST"])
+def add_students():
+    req = request.json
+    student_id = req["student_id"]
+    first_name = req["first_name"]
+    last_name = req["last_name"]
+    gender = req["gender"]
+    year_level = req["year_level"]
+    course_code = req["course_code"]
+    print(student_id, first_name, last_name, gender, year_level, course_code)
+    create_student = students_model.add_student(
+        student_id, first_name, last_name, gender, year_level, course_code
+    )
+    message = create_student[0]
+    status_code = create_student[1]
+
+    return message, status_code
