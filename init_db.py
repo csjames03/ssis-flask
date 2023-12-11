d14b4ce6decf894ac869158e630c4554e14c70dc
csjames03 <csjames0303@gmail.com>
2023-12-11 17:27:01 +0800

    Populate the db:

--- init_db.py
+++ init_db.py
@@ -21,82 +21,94 @@ def initialize_database():
         cursor = connection.cursor()
 
         sql_script = """
-        DROP DATABASE IF EXISTS `student-information-system`;
-        CREATE DATABASE IF NOT EXISTS `student-information-system`;
-        USE `student-information-system`;
-        CREATE TABLE IF NOT EXISTS college (
-            college_code VARCHAR(20) PRIMARY KEY,
-            college_name VARCHAR(100) NOT NULL
-        );
-        CREATE TABLE IF NOT EXISTS course (
-            course_code VARCHAR(20) PRIMARY KEY,
-            course_name VARCHAR(200) NOT NULL,
-            college_code VARCHAR(20) NOT NULL,
-            FOREIGN KEY (college_code) REFERENCES college(college_code) ON DELETE CASCADE ON UPDATE CASCADE
-        );
-        CREATE TABLE IF NOT EXISTS student (
-            student_id CHAR(9) PRIMARY KEY,
-            first_name VARCHAR(100) NOT NULL,
-            last_name VARCHAR(100) NOT NULL,
-            course_code VARCHAR(20) NOT NULL,
-            year_level VARCHAR(3) NOT NULL,
-            sex VARCHAR(10) NOT NULL,
-            FOREIGN KEY (course_code) REFERENCES course(course_code) ON DELETE CASCADE ON UPDATE CASCADE
-        );
+        DROP DATABASE IF EXISTS `student-information-system-v1`;
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `student-information-system-v1`;
+
+-- Switch to the created database
+USE `student-information-system-v1`;
+
+-- Create the college table
+CREATE TABLE IF NOT EXISTS college (
+    college_code VARCHAR(20) PRIMARY KEY,
+    college_name VARCHAR(100) NOT NULL
+);
+
+-- Create the course table
+CREATE TABLE IF NOT EXISTS course (
+    course_code VARCHAR(20) PRIMARY KEY,
+    course_name VARCHAR(200) NOT NULL,
+    college_code VARCHAR(20) NOT NULL,
+    FOREIGN KEY (college_code) REFERENCES college(college_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Create the student table
+CREATE TABLE IF NOT EXISTS student (
+    student_id CHAR(9) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    course_code VARCHAR(20) NOT NULL,
+    year_level VARCHAR(3) NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    FOREIGN KEY (course_code) REFERENCES course(course_code) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
         INSERT INTO college (college_code, college_name) VALUES
-        ('CCS', 'College of Computer Studies'),
-        ('CON', 'College of Nursing'),
-        ('CED', 'College of Education'),
-        ('COE', 'College of Engineering'),
-        ('CASS', 'College of Arts and Social Sciences'),
-        ('CBA', 'College of Business Administration'),
-        ('CAL', 'College of Arts and Letters'),
-        ('CNSM', 'College of Natural Sciences and Mathematics'),
-        ('CHS', 'College of Health Sciences'),
-        ('CSSP', 'College of Social Sciences and Philosophy'),
-        ('CDC', 'College of Development Communication'),
-        ('CEM', 'College of Economics and Management'),
-        ('CHE', 'College of Home Economics'),
-        ('CVM', 'College of Veterinary Medicine'),
-        ('CPAFS', 'College of Public Affairs and Development'),
-        ('CSNR', 'College of Science and Natural Resources'),
-        ('CHK', 'College of Human Kinetics'),
-        ('CP', 'College of Public Health'),
-        ('CFNR', 'College of Forestry and Natural Resources'),
-        ('CPG', 'College of Public Governance'),
-        ('CA', 'College of Agriculture'),
-        ('CMC', 'College of Mass Communication'),
-        ('CN', 'College of Nursing'),
-        ('CTE', 'College of Teacher Education'),
-        ('CSS', 'College of Social Sciences'),
-        ('CPAF', 'College of Public Affairs'),
-        ('CD', 'College of Dentistry'),
-        ('CM', 'College of Medicine'),
-        ('CAB', 'College of Arts and Business'),
-        ('CES', 'College of Environmental Science'),
-        ('CFAD', 'College of Fine Arts and Design'),
-        ('CPHM', 'College of Public Health and Medicine'),
-        ('CCA', 'College of Communication and Arts'),
-        ('CAFA', 'College of Architecture and Fine Arts'),
-        ('CCT', 'College of Communication and Technology'),
-        ('CPH', 'College of Public Health'),
-        ('CPS', 'College of Political Science'),
-        ('CLaw', 'College of Law'),
-        ('CMBA', 'College of MBA'),
-        ('CIA', 'College of Information and Arts'),
-        ('CHTM', 'College of Hotel and Tourism Management'),
-        ('CBAA', 'College of Business Administration and Accountancy'),
-        ('CEA', 'College of Engineering and Architecture'),
-        ('CDM', 'College of Digital Marketing'),
-        ('CHM', 'College of Hospitality Management'),
-        ('CJ', 'College of Journalism'),
-        ('CSIT', 'College of Science and Information Technology'),
-        ('CMIS', 'College of Management and Information System');
+('CCS', 'College of Computer Studies'),
+('CON', 'College of Nursing'),
+('CED', 'College of Education'),
+('COE', 'College of Engineering'),
+('CASS', 'College of Arts and Social Sciences'),
+('CBA', 'College of Business Administration'),
+('CAL', 'College of Arts and Letters'),
+('CNSM', 'College of Natural Sciences and Mathematics'),
+('CHS', 'College of Health Sciences'),
+('CSSP', 'College of Social Sciences and Philosophy'),
+('CDC', 'College of Development Communication'),
+('CEM', 'College of Economics and Management'),
+('CHE', 'College of Home Economics'),
+('CVM', 'College of Veterinary Medicine'),
+('CPAFS', 'College of Public Affairs and Development'),
+('CSNR', 'College of Science and Natural Resources'),
+('CHK', 'College of Human Kinetics'),
+('CP', 'College of Public Health'),
+('CFNR', 'College of Forestry and Natural Resources'),
+('CPG', 'College of Public Governance'),
+('CA', 'College of Agriculture'),
+('CMC', 'College of Mass Communication'),
+('CN', 'College of Nursing'),
+('CTE', 'College of Teacher Education'),
+('CSS', 'College of Social Sciences'),
+('CPAF', 'College of Public Affairs'),
+('CD', 'College of Dentistry'),
+('CM', 'College of Medicine'),
+('CAB', 'College of Arts and Business'),
+('CES', 'College of Environmental Science'),
+('CFAD', 'College of Fine Arts and Design'),
+('CPHM', 'College of Public Health and Medicine'),
+('CCA', 'College of Communication and Arts'),
+('CAFA', 'College of Architecture and Fine Arts'),
+('CCT', 'College of Communication and Technology'),
+('CPH', 'College of Public Health'),
+('CPS', 'College of Political Science'),
+('CLaw', 'College of Law'),
+('CMBA', 'College of MBA'),
+('CIA', 'College of Information and Arts'),
+('CHTM', 'College of Hotel and Tourism Management'),
+('CBAA', 'College of Business Administration and Accountancy'),
+('CEA', 'College of Engineering and Architecture'),
+('CDM', 'College of Digital Marketing'),
+('CHM', 'College of Hospitality Management'),
+('CJ', 'College of Journalism'),
+('CSIT', 'College of Science and Information Technology'),
+('CMIS', 'College of Management and Information System')
+ON DUPLICATE KEY UPDATE
+college_name = VALUES(college_name);
 
 
         INSERT INTO course (course_code, course_name, college_code) VALUES
-         ('BSCS', 'Computer Science', 'CCS'),
+        ('BSCS', 'Computer Science', 'CCS'),
         ('BSCA', 'Computer Applications', 'CCS'),
         ('BSIT', 'Information Technology', 'CCS'),
         ('BSIS', 'Information System', 'CCS'),
@@ -138,7 +150,9 @@ def initialize_database():
         ('LING', 'Linguistics', 'CAL'), 
         ('FILM', 'Film Studies', 'CASS'), 
         ('FOREX', 'Foreign Exchange Management', 'CBA'), 
-        ('HIST', 'History', 'CSSP');
+        ('HIST', 'History', 'CSSP')
+        ON DUPLICATE KEY UPDATE
+course_code = VALUES(course_code);
 
 
 INSERT INTO student (student_id, first_name, last_name, course_code, year_level, sex) VALUES
@@ -173,8 +187,9 @@ INSERT INTO student (student_id, first_name, last_name, course_code, year_level,
     ('2023-2027', 'Abigail', 'Lee', 'URB', '3rd', 'Female'),
     ('2023-2028', 'Benjamin', 'Brown', 'TECH', '4th', 'Male'),
     ('2023-2029', 'Catherine', 'Miller', 'STAT', '1st', 'Female'),
-    ('2023-2030', 'Daniel', 'Johnson', 'CMSC', '2nd', 'Male');
-    -- Add more records as neededVALUES
+    ('2023-2030', 'Daniel', 'Johnson', 'CMSC', '2nd', 'Male')
+    ON DUPLICATE KEY UPDATE
+student_id = VALUES(student_id);
         
 
 
